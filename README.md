fda1ff9657bbfdacd9e4bb32441ae902f048563e
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2023-12-15 15:40:31 -0300

    update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # 20232BSET03P2
 Inteli - Engenharia de Software | Avaliação 2023-2B P2
+
+## Tratativas
+Foram identificados erros nos IDs das tabelas, foi implementado o autoincrimento de ID.
+
+Não era verificado o conteudo das requisições recebidas, agora temos uma verificação do tipo do dado recebido e de tamanho, para garantir que não seja colocado algo que não seja o nome de um animal.
+
+Era possível criar diversos animais com o mesmo nome, foi implementado uma segurança de réplicas, garantindo que apenas haja uma versão do dado no sistema.
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const db = new sqlite3.Database(':memory:');
 
 db.serialize(() => {
   db.run("CREATE TABLE if NOT EXISTS cats (id integer primary key, name TEXT unique, votes INT)");
-  db.run("CREATE TABLE if NOT EXISTS dogs (id INT, name TEXT unique, votes INT)");
+  db.run("CREATE TABLE if NOT EXISTS dogs (id integer primary key, name TEXT unique, votes INT)");
 });
 
 app.post('/cats', (req, res) => {
@@ -19,7 +19,7 @@ app.post('/cats', (req, res) => {
   if(name != null && typeof(name) == typeof('') && name.length < 20) {
     db.run(`INSERT INTO cats (name, votes) VALUES ('${name}', 0)`, function(err) {
       if (err) {
-        res.status(500).send("Erro ao inserir no banco de dados");
+        res.status(500).send("Erro ao tratar sua solicitação.");
       } else {
         res.status(201).json({name, votes: 0 });
       }
@@ -33,7 +33,7 @@ app.post('/dogs', (req, res) => {
   if(name != null && typeof(name) == typeof('') && name.length < 20) {
     db.run(`INSERT INTO dogs (name, votes) VALUES ('${name}', 0)`, function(err) {
       if (err) {
-        res.status(500).send("Erro ao inserir no banco de dados");
+        res.status(500).send("Erro ao tratar sua solicitação.");
       } else {
         res.status(201).json({name, votes: 0 });
       }
@@ -44,7 +44,7 @@ app.post('/dogs', (req, res) => {
 app.post('/vote/:animalType/:id'), (req, res) => {
   db.all(`SELECT FROM '${animalType}' WHERE id = " + ${id}`, [], (err, rows) => {
     if (err) {
-      res.status(500).send("Erro ao consultar o banco de dados");
+      res.status(500).send("Erro ao processar sua solicitação.");
     } else {
       db.run(`UPDATE ${animalType} SET votes = votes + 1 WHERE id = ${id}`);
       res.status(200).send("Voto computado");
@@ -55,7 +55,7 @@ app.post('/vote/:animalType/:id'), (req, res) => {
 app.get('/cats', (req, res) => {
   db.all("SELECT * FROM cats", [], (err, rows) => {
     if (err) {
-      res.status(500).send("Erro ao consultar o banco de dados");
+      res.status(500).send("Erro ao processar sua solicitação.");
     } else {
       res.json(rows);
     }
@@ -65,7 +65,7 @@ app.get('/cats', (req, res) => {
 app.get('/dogs', (req, res) => {
   db.all("SELECT * FROM cats", [], (err, rows) => {
     if (err) {
-      res.status(500).send("Erro ao consultar o banco de dados");
+      res.status(500).send("Erro ao processar sua solicitação");
     } else {
       res.json(rows);
     }
